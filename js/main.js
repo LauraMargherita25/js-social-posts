82448eb04fa57b7af6e03ef3dc49c29b83bb672f
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-03-08 15:55:18 +0100

    Linked obj property to post

--- js/main.js
+++ js/main.js
@@ -65,32 +65,32 @@ const elePost = document.createElement("div");
 elePost.classList.add("post");
 elePost.innerHTML = `
     <div class="post__header">
-    <div class="post-meta">                    
-        <div class="post-meta__icon">
-            <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${posts[0].author.image}" alt="Phil Mangione">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${posts[0].author.name}</div>
+                <div class="post-meta__time">4 mesi fa</div>
+            </div>                    
         </div>
-        <div class="post-meta__data">
-            <div class="post-meta__author">Phil Mangione</div>
-            <div class="post-meta__time">4 mesi fa</div>
-        </div>                    
     </div>
-    </div>
-    <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
-    <div class="post__image">
-    <img src="https://unsplash.it/600/300?image=171" alt="">
-    </div>
-    <div class="post__footer">
-    <div class="likes js-likes">
-        <div class="likes__cta">
-            <a class="like-button  js-like-button" href="#" data-postid="1">
-                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                <span class="like-button__label">Mi Piace</span>
-            </a>
+        <div class="post__text">${posts[0].content}</div>
+        <div class="post__image">
+        <img src="${posts[0].media}" alt="">
         </div>
-        <div class="likes__counter">
-            Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
-        </div>
-    </div> 
+        <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${posts[0].likes}</b> persone
+            </div>
+        </div> 
     </div> 
 `;
 eleContainer.append(elePost);
